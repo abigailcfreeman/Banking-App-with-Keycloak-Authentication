0a83ecac2006fa1a544fef47e07d56c350e92592
abigailcfreeman <abigailcfreemandev@gmail.com>
2022-10-25 12:20:29 +0100

    mvn

--- src/main/java/com/ironhack/hellokeycloak/HelloKeycloakApplication.java
+++ src/main/java/com/ironhack/hellokeycloak/HelloKeycloakApplication.java
@@ -3,12 +3,11 @@ package com.ironhack.hellokeycloak;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class HelloKeycloakApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(HelloKeycloakApplication.class, args);
-	}
-
-}
-
+//@SpringBootApplication
+//public class HelloKeycloakApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(HelloKeycloakApplication.class, args);
+//	}
+//
+//}
