fe2a48a58321d4da710b6bdb8062507d9231a6f5
abigailcfreeman <abigailcfreemandev@gmail.com>
2022-10-25 12:29:45 +0100

    mvn

--- src/test/java/com/ironhack/hellokeycloak/HelloKeycloakApplicationTests.java
+++ src/test/java/com/ironhack/hellokeycloak/HelloKeycloakApplicationTests.java
@@ -3,15 +3,6 @@ package com.ironhack.hellokeycloak;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
-//class HelloKeycloakApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
-//
-//}
-
 @SpringBootTest
 class HelloKeycloakApplicationTests{
 
